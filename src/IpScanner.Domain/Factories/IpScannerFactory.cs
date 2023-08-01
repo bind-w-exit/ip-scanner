170973fcd4bf20d31f3f7c1bd2394e1b4752c057
Vitaliy <bezuglivita123@gmail.com>
2023-08-01 12:00:50 +0300

    factory refactored

--- src/IpScanner.Domain/Factories/IpScannerFactory.cs
+++ src/IpScanner.Domain/Factories/IpScannerFactory.cs
@@ -1,60 +1,37 @@
 ﻿using System.Net;
-using System.Linq;
+using IpScanner.Domain.Models;
 using System.Collections.Generic;
 using IpScanner.Domain.Exceptions;
 using IpScanner.Domain.Interfaces;
 using IpScanner.Domain.Validators;
-using System;
-using IpScanner.Domain.Models;
 
 namespace IpScanner.Domain.Factories
 {
     public class IpScannerFactory : IIpScannerFactory
     {
-        private readonly IMacAddressRepository _macAddressScanner;
-        private readonly IManufactorRepository _manufactorReceiver;
+        private readonly IHostRepository _hostRepository;
+        private readonly IMacAddressRepository _macAddressRepository;
+        private readonly IManufactorRepository _manufactorRepository;
         private readonly IValidator<IpRange> _ipRangeValidator;
 
-        public IpScannerFactory(IMacAddressRepository macAddressScanner, 
-            IManufactorRepository manufactorReceiver, IValidator<IpRange> ipRangeValidator)
+        public IpScannerFactory(IMacAddressRepository macAddressScanner, IManufactorRepository manufactorReceiver,
+            IHostRepository hostRepository, IValidator<IpRange> ipRangeValidator)
         {
-            _macAddressScanner = macAddressScanner;
-            _manufactorReceiver = manufactorReceiver;
+            _hostRepository = hostRepository;
+            _macAddressRepository = macAddressScanner;
+            _manufactorRepository = manufactorReceiver;
             _ipRangeValidator = ipRangeValidator;
         }
 
-        public Models.NetworkScanner CreateBasedOnIpRange(IpRange range)
+        public NetworkScanner CreateBasedOnIpRange(IpRange range)
         {
             if (_ipRangeValidator.Validate(range) == false)
             {
                 throw new IpValidationException();
             }
 
-            var ips = GenerateIPAddresses(range.Range);
-            return new Models.NetworkScanner(ips, _manufactorReceiver, _macAddressScanner);
-        }
-
-        public List<IPAddress> GenerateIPAddresses(string ipRange)
-        {
-            return ipRange.Split(',')
-                .SelectMany(range => GenerateIPAddressesForRange(range.Trim()))
-                .ToList();
-        }
-
-        private IEnumerable<IPAddress> GenerateIPAddressesForRange(string ipRange)
-        {
-            string[] parts = ipRange.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
-            string networdId = string.Join('.', parts.Take(3));
-
-            string[] lastPart = parts[3].Split('-');
-
-            int start = int.Parse(lastPart[0]);
-            int end = lastPart.Length > 1 ? int.Parse(lastPart[1]) : start;
-
-            var ipAddressesForRange = Enumerable.Range(start, end - start + 1)
-                .Select(i => IPAddress.Parse(networdId + '.' + i));
-
-            return ipAddressesForRange;
+            List<IPAddress> ips = range.GenerateIPAddresses();
+            return new NetworkScanner(ips, _manufactorRepository, _macAddressRepository, _hostRepository);
         }
     }
 }
--- src/IpScanner.Domain/Models/IpRange.cs
+++ src/IpScanner.Domain/Models/IpRange.cs
@@ -1,4 +1,9 @@
-﻿namespace IpScanner.Domain.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace IpScanner.Domain.Models
 {
     public class IpRange
     {
@@ -8,5 +13,28 @@
         }
 
         public string Range { get; }
+
+        public List<IPAddress> GenerateIPAddresses(IpRange ipRange)
+        {
+            return ipRange.Range.Split(',')
+                .SelectMany(range => GenerateIPAddressesForRange(range.Trim()))
+                .ToList();
+        }
+
+        private IEnumerable<IPAddress> GenerateIPAddressesForRange(string ipRange)
+        {
+            string[] parts = ipRange.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            string networdId = string.Join('.', parts.Take(3));
+
+            string[] lastPart = parts[3].Split('-');
+
+            int start = int.Parse(lastPart[0]);
+            int end = lastPart.Length > 1 ? int.Parse(lastPart[1]) : start;
+
+            var ipAddressesForRange = Enumerable.Range(start, end - start + 1)
+                .Select(i => IPAddress.Parse(networdId + '.' + i));
+
+            return ipAddressesForRange;
+        }
     }
 }
