72bb41d4f3bbc074639408f8f687ed2076569dfa
Vitaliy <bezuglivita123@gmail.com>
2023-08-02 22:06:44 +0300

    added ui elements

---
+++ src/IpScanner.Ui/Convertors/StringContentToVisibility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace IpScanner.Ui.Convertors
+{
+    public class StringContentToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string content = value as string;
+
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                return Windows.UI.Xaml.Visibility.Visible;
+            }
+
+            return Windows.UI.Xaml.Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
