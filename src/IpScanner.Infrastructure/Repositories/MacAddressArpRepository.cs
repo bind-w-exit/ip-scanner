16096569ea1d103803f2485772d1d289bcc6b28d
Vitaliy <bezuglivita123@gmail.com>
2023-08-09 15:03:21 +0300

    NetworkScanner fixed exception arch

--- src/IpScanner.Infrastructure/Repositories/MacAddressArpRepository.cs
+++ src/IpScanner.Infrastructure/Repositories/MacAddressArpRepository.cs
@@ -17,12 +17,8 @@ namespace IpScanner.Infrastructure.Repositories
                 byte[] macAddr = new byte[6];
                 uint macAddrLen = (uint)macAddr.Length;
 
-                if (SendARP(BitConverter.ToInt32(destination.GetAddressBytes(), 0), 0, macAddr, ref macAddrLen) != 0)
-                {
-                    throw new MacAddressNotFoundException(destination);
-                }
-
-                return new PhysicalAddress(macAddr);
+                bool deviceFound = SendARP(BitConverter.ToInt32(destination.GetAddressBytes(), 0), 0, macAddr, ref macAddrLen) == 0;
+                return deviceFound ? new PhysicalAddress(macAddr) : PhysicalAddress.None;
             });
         }
 
