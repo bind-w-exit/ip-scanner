07029cb8f2aaa169a464d1ddc2fa569279ea5c9b
Vitaliy <bezuglivita123@gmail.com>
2023-08-10 12:04:07 +0300

    modified toolbar

--- src/IpScanner.Ui/MainPage.xaml.cs
+++ src/IpScanner.Ui/MainPage.xaml.cs
@@ -1,12 +1,20 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
 using IpScanner.Ui.Pages;
 using IpScanner.Ui.ViewModels;
+using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Media;
 
 namespace IpScanner.Ui
 {
     public sealed partial class MainPage : Page
     {
+        private readonly CoreApplicationViewTitleBar coreTitleBar;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -14,8 +22,45 @@ namespace IpScanner.Ui
             InitializeScanPageViewModelForMessanger();
             DataContext = Ioc.Default.GetService<MainPageViewModel>();
             ContentFrame.Navigate(typeof(ScanPage));
+
+            // Hide default title bar.
+            coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            // Set caption buttons background to transparent.
+            ApplicationViewTitleBar titleBar =
+                ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+
+            // Set XAML element as a drag region.
+            Window.Current.SetTitleBar(AppTitleBar);
+
+            // Register a handler for when the size of the overlaid caption control changes.
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            // Register a handler for when the title bar visibility changes.
+            // For example, when the title bar is invoked in full screen mode.
+            coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
         }
 
         private void InitializeScanPageViewModelForMessanger() => Ioc.Default.GetService<ScanPageViewModel>();
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            LeftPaddingColumn.Width = new GridLength(coreTitleBar.SystemOverlayLeftInset);
+            RightPaddingColumn.Width = new GridLength(coreTitleBar.SystemOverlayRightInset);
+        }
+
+        private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (sender.IsVisible)
+            {
+                AppTitleBar.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                AppTitleBar.Visibility = Visibility.Collapsed;
+            }
+        }
     }
 }
